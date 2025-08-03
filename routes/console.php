12ca18bc41cfed2ba3f5af57b86b94ef3f25164d
Lucas John <lucasjohn414@gmail.com>
2025-08-03 16:25:17 +0300

    Add command to deactivate expired kifurushi purchases
    Introduces a new console command that disables kifurushi purchases whose end_date has passed. Schedules the command to run daily at midnight via the console route.

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Artisan::command('schedule:custom', function () {
+    // This is optional; for manual testing.
+})->purpose('Run custom schedule');
+
+return function (Schedule $schedule) {
+    $schedule->command('kifurushi:deactivate-expired')->dailyAt('00:00');
+};
